5937604bb2d170bf055ae2d83a7ebf0e6be36043
padure <paduregheorghe7@gmail.com>
2024-09-11 10:17:42 +0300

    Laborator 2

--- laborator2/index.php
+++ laborator2/index.php
@@ -5,5 +5,16 @@
         return $nr1 + $nr2;
     }
     echo suma(34, 23);
+    # Anonima | Constanta
+    $salut = function(string $nume):string{
+        return "Salut, $nume";
+    };
+    echo $salut("Ion");
+    
+    $numeUser="John Doe";
+    $hello = function ($text) use ($numeUser) : string {
+        return "$text, $numeUser";
+    };
+    echo $hello("Hello");
     # 
 ?>
