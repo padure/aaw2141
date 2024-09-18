09ba8a714bd66adf2d8708ef8107c59f3e965ea4
padure <paduregheorghe7@gmail.com>
2024-09-18 11:04:45 +0300

    Laborator 6

--- laborator6/index.php
+++ laborator6/index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imagini</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <div class="container">
         <h4 class="my-4">Incarca o imagine</h4>
@@ -17,5 +19,37 @@
             <button type="submit" class="btn btn-dark">Save</button>
         </form>
     </div>
+    <div class="col-10 my-4 m-auto">
+        <h4>Lista imaginilor din sistem</h4>
+        <?php
+        $imagini = scandir('uploads');
+        $imagini = array_diff($imagini, [".", ".."]);
+        ?>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <td>Imagine</td>
+                    <td>Optiuni</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($imagini as $imagine): ?>
+                    <tr>
+                        <td>
+                            <img src="uploads/<?= $imagine; ?>" alt="Imagine" class="col-1 m-0">
+                        </td>
+                        <td>
+                            <a  href="download.php?file=<?=$imagine?>"
+                                class="text-success">Descarca</a>
+                            <a  href="delete.php?file=<?=$imagine?>"
+                                class="text-danger"
+                                onclick="return confirm('Sigur doriti sa stergeti aceasta imagine?');">Sterge</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
--- laborator6/upload.php
+++ laborator6/upload.php
@@ -9,6 +9,16 @@
             $file_size = $_FILES['file']['size'];
             $file_tmp = $_FILES['file']['tmp_name'];
             $file_ext = pathinfo(basename($file_name), PATHINFO_EXTENSION);
+            $extensii = ["jpeg", "jpg", "png", "gif"];
+            
+            if(!in_array($file_ext, $extensii)){
+                die("Selectati o imagine!");
+            }
+
+            if($file_size > 1 * 1024 * 1024){
+                die("Imagine care depaseste marimea admisibila!");
+            }
+
             $patch = "uploads/" . uniqid('img_', true) . ".".$file_ext;
             if( !move_uploaded_file($file_tmp,  $patch)){
                 die('Eroare la incarcare');
