0c68e408a8a9175eb1dc474df2caa296ca325414
padure <paduregheorghe7@gmail.com>
2024-10-22 10:44:01 +0300

    Teorie 8

--- teorie8/edit.php
+++ teorie8/edit.php
@@ -6,6 +6,13 @@
         $stmt = $pdo->prepare($sql);
         $stmt->execute(['id' => $id]);
         $user = $stmt->fetch();
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $numePrenume = $_POST['nume_prenume'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $dataNasterii = $_POST['data_nasterii'];
+            $sql = "UPDATE users SET nume_prenume = :nume_prenume, email = :email, parola = :parola, data_nasterii = :data_nasterii WHERE id = :id";
+        }
         $pdo = null;
     } catch (PDOException $e) {
         die("Eroare la conexiune. " . $e->getMessage());
@@ -32,17 +39,17 @@
                     </div>
                     <div class="mb-3">
                         <label for="email">Email</label>
-                        <input type="email" name="email" id="email" class="form-control">
+                        <input type="email" name="email" id="email" class="form-control" value="<?=$user['email']?>">
                     </div>
                     <div class="mb-3">
                         <label for="password">Parola</label>
-                        <input type="password" name="password" id="password" class="form-control">
+                        <input type="password" name="password" id="password" class="form-control" value="<?=$user['parola']?>">
                     </div>
                     <div class="mb-3">
                         <label for="data_nasterii">Data nasterii</label>
-                        <input type="date" name="data_nasterii" id="data_nasterii" class="form-control">
+                        <input type="date" name="data_nasterii" id="data_nasterii" class="form-control" value="<?=$user['data_nasterii']?>">
                     </div>
-                    <button class="btn btn-dark btn-sm" type="submit">Save</button>
+                    <button class="btn btn-warning text-white btn-sm" type="submit">Edit</button>
                 </form>
             </div>
         </div>
