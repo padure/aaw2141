434061eabd1e64042ed06278af731fd929eb8cab
padure <paduregheorghe7@gmail.com>
2024-10-22 10:57:37 +0300

    Teorie 8

--- teorie8/delete.php
+++ teorie8/delete.php
@@ -1,7 +1,11 @@
 <?php
     try {
         require_once './connect.php';
-        
+        $id = htmlspecialchars($_GET['id']);
+        $sql = "DELETE FROM users WHERE id = :id";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['id' => $id]);
+        header('location:index.php');
     } catch (PDOException $e) {
         die("Eroare la conexiune. " . $e->getMessage());
     }
