4571e161566193469cc32c102f65031056d602d3
padure <paduregheorghe7@gmail.com>
2024-11-07 11:00:22 +0200

    Laborator 11

--- laborator11/routes/web.php
+++ laborator11/routes/web.php
@@ -3,6 +3,7 @@
     use Psr\Http\Message\ServerRequestInterface as Request;
     //Controllers
     use App\Controllers\ProjectController;
+    use App\Controllers\EnvironmentController;
     //Index
     $app->get('/', function (Request $request, Response $response, array $args) {
         $response->getBody()->write("Hello!");
@@ -14,3 +15,9 @@
     $app->post('/projects', [ProjectController::class, 'store']);
     $app->put('/projects/{id}', [ProjectController::class, 'update']);
     $app->delete('/projects/{id}', [ProjectController::class, 'delete']);
+    //Environment
+    $app->get('/environments', [EnvironmentController::class, 'index']);
+    $app->get('/environments/{id}', [EnvironmentController::class, 'show']);
+    $app->post('/environments', [EnvironmentController::class, 'store']);
+    $app->put('/environments/{id}', [EnvironmentController::class, 'update']);
+    $app->delete('/environments/{id}', [EnvironmentController::class, 'delete']);
