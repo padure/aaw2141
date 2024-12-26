8fc16164f305ecb2b4a392d1283a183f0a1c5a76
padure <paduregheorghe7@gmail.com>
2024-12-26 12:56:15 +0200

    Examen

--- laborator15/views/index.view.php
+++ laborator15/views/index.view.php
@@ -12,11 +12,17 @@
     <div class="container py-4">
         <h5>Lista masinelor</h5>
         <?php foreach($cars as $car): ?>
-            <div class="border p-2 bg-light d-flex mb-2">
-                <?= $car->name; ?>
-                <?= $car->model; ?>
-                <?= $car->year; ?>
-                <?= $car->engine_type; ?>
+            <div class="border p-2 bg-light d-flex mb-2 justify-content-between">
+                <div>
+                    <?= $car->name; ?>
+                    <?= $car->model; ?>
+                    <?= $car->year; ?>
+                    <?= $car->engine_type; ?>
+                </div>
+                <div class="d-flex gap-2">
+                    <a href="/cars/<?=$car->id?>/edit" class="text-warning">Edit</a>
+                    <a href="/cars/<?=$car->id?>" class="text-dark">Show</a>
+                </div>
             </div>
         <?php endforeach; ?>
     </div>
